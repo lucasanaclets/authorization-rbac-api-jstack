356e310ae398731d8e1edf36182d4ef675f4e17e
Lucas Anacleto <lucasanacleto04@gmail.com>
2025-10-03 14:35:37 -0300

    Estrutura base do projeto para estudo de Autorização Baseada em Cargos (RBCA) em API Node.js

---
+++ src/server/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+
+import { routeAdapter } from "./adapters/routeAdapter";
+
+import { makeSignUpController } from "../factories/makeSignUpController";
+import { makeSignInController } from "../factories/makeSignInController";
+import { makeListLeadsController } from "../factories/makeListLeadsController";
+import { middlewareAdapter } from "./adapters/middlewareAdapter";
+import { makeAuthenticationMiddleware } from "../factories/makeAuthenticationMiddleware";
+
+const app = express();
+
+// Middleware - Body Parse
+app.use(express.json());
+
+app.post("/sign-up", routeAdapter(makeSignUpController()));
+app.post("/sign-in", routeAdapter(makeSignInController()));
+
+app.get(
+  "/leads",
+  middlewareAdapter(makeAuthenticationMiddleware()),
+  routeAdapter(makeListLeadsController())
+);
+
+app.listen(3001, () =>
+  console.log("🔥 Server started at http://localhost:3001")
+);
